1c62abb2f45ee50038c5bedf0b35bfd4d6a7abb2
1238qwer <1238qwer@naver.com>
2018-10-16 14:57:53 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import MyName from "./MyName";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>리액트</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
